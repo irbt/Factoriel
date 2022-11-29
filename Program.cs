7d10abe11e0f585281391a814457026eaa85cb5c
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-29 21:40:56 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Given a non-negative integer, write a method that returns factorial of a number.
+
+namespace Program{
+    class FactorialTask{
+        public static void Main(string [] args){
+            Console.Clear();
+            static int FactorialTask(int number){
+                
+                return  ;
+            }
+        Console.WriteLine (Factorial(456));// → 120
+        Console.WriteLine (Factorial(1));// → 6
+        }
+    }
+}
